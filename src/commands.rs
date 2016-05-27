9dc2ef468f8280ec6eb38cfa68da7baa24b894f3
Roy Brunton <roy@flashtalking.com>
2016-05-27 11:23:15 +0100

    tidied up match statement in commands

--- src/commands.rs
+++ src/commands.rs
@@ -66,7 +66,7 @@ impl Command {
             'F' => Command::Prev(Box::new(Command::Nil)),
             '?' => Command::Con,
             '!' => Command::NCon,
-            '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => Command::Int(inp as u64),
+            '0'...'9' => Command::Int(inp as u64),
             'v' => Command::VInt,
             '.' => Command::Rep,
             '(' => Command::LP,
